7bbc403de335d2688878256234fa495a97a73604
Damini Varu <damvaru4@gmail.com>
2019-09-26 14:25:20 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,7 @@
 class Shoe
   
-  attr_accessor :color, :material, :condition
-  attr_reader :brand, :size
+  attr_accessor :color, :size :material, :condition
+  attr_reader :brand
 
   BRANDS = []
   
