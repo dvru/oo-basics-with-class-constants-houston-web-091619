53ace0f257e19910d6c830af60d97a6710326e6b
Damini Varu <damvaru4@gmail.com>
2019-09-26 14:14:39 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,19 +2,19 @@ class Book
   attr_accessor :author, :page_count, :genre
   attr_reader :title
 
-  @@all = []
+  # @@all = []
 
   def initialize(title, author, page_count, genre)
     @title = title
     @author = author
     @page_count = page_count
     @genre = genre
-    @@all << self
+    # @@all << self
   end
   
-  def self.all
-    @@all
-  end
+  # def self.all
+  #   @@all
+  # end
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
