24bf6f95e2a6f415d4806b0b9ea89a8d2bf438de
Damini Varu <damvaru4@gmail.com>
2019-09-26 14:20:57 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,12 +7,13 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-  end
-  
-  def brand=(brand)
-    @brand = brand
     BRANDS << brand 
   end
+  
+  # def brand=(brand)
+  #   @brand = brand
+  #   BRANDS << brand 
+  # end
   
 
   def cobble
